5ce09f81f6d9d97894a9fd9baedbfebb872f54cb
kuewu pamela <kuewupamela@gmail.com>
2024-06-04 14:28:52 +0000

    update

--- eddys pizza/New folder/home.java
+++
@@ -1,8 +0,0 @@
-public class home{
-
-    public static void home(String[] args) {
-        int num = 76;
-        int hal = 56;
-        double multi = (num*hal);
-        system.out.println(multi);
-    
--- eddys pizza/New folder/webs/main.java
+++
@@ -1,8 +0,0 @@
-public class exclusive {
-
-	public static void main(String[] args) {
-//		Data types and variables
-		int num = 5;
-	
-	}
-     }
