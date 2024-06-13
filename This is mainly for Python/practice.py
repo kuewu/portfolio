2bdf5b4f6141a48a7406214d64af26f01e2107c0
kuewu pamela <kuewupamela@gmail.com>
2024-06-13 08:30:21 +0000

    update

--- This is mainly for Python/practice.py
+++ This is mainly for Python/practice.py
@@ -1,1 +1,30 @@
-print("helo world")
+import sys
+print(sys.version)
+
+
+#hello guys we are trying to explore python as beginners.
+
+
+if 5 > 2:
+    print("Five is greater than two")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+# Lets perform some casting. Always note that casting is used to specify the data type you want to use
+m = str(3)
+n = int(3)
+o = float(3)
+print(m)
+print(n)
+print(o)
+
+
+# Lets get the data type
+q = 5
+r = "alii"
+print(type(q))
+print(type(r))
