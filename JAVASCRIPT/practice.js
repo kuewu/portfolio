0c4fd7d43a00a35836e82ba3828ba84236d83552
kuewu pamela <kuewupamela@gmail.com>
2024-06-15 15:47:31 +0000

    update

---
+++ JAVASCRIPT/practice.js
@@ -0,0 +1,25 @@
+// lets add variables
+// Variables const. let
+let name = "pamela Kuewu"
+const age = 20
+const student = false
+name = "mummyyy"
+const myname = null
+const studentlist = ["name", "pamela"]
+
+//operators
+
+//basic operators in javascript
+
+const a = 10
+const b = 10
+const sumAandB = a - b
+const mulAandB = a * b
+
+
+console.log(sumAandB);
+console.log(mulAandB % sumAandB);
+//Data types
+//string 
+//boolean
+//number
