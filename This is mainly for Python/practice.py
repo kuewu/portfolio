6ae047a915c07d8ef2e5f8d6f01d5f17ab300759
kuewu pamela <kuewupamela@gmail.com>
2024-06-19 11:03:03 +0000

    update

--- This is mainly for Python/practice.py
+++ This is mainly for Python/practice.py
@@ -4,62 +4,20 @@ print(sys.version)
 
 #hello guys we are trying to explore python as beginners.
 
-print("pamela")
-if 5 > 2:
-    print("Five is greater than two")
+#
 
-x = 5
-y = "John"
-print(x)
-print(y)
+#type conversion
+x = 1
+y = 2.8
+z = 1j
 
+a = float(x)
+b = int(y)
+c = complex(x)
 
-# Lets perform some casting. Always note that casting is used to specify the data type you want to use
-m = str(3)
-n = int(3)
-o = float(3)
-print(m)
-print(n)
-print(o)
+print(a)
+print(b)
+print(c)
 
-
-# Lets get the data type
-q = 5
-r = "alii"
-print(type(q))
-print(type(r))
-
-
-
-s, t, u = "pamela", "Aunty", "Madam" 
-print(s)
-print(t)
-print(u)
-
-"""class student:
-    name = str
-    age = int
-    year = str
-
-pamela = student(name = "pamela", age= 16, year ="1983")
-kuewu = student(name = "kuewu", age= 16 ,year ="1983")
-john = student(name = "john", age= 16 ,year ="1983")
-racheal = student(name = "racheal", age= 16, year ="1983")"""
-
-fruits = ["apple", "orange", "banana"]
-p, j, k = fruits
-print(p, j, k)
-print(n+q)
-
-
-# Global variable
-###myfunc()
-
-# another example
-true = "my own"
-
-def myfunction():
-    true = "not mine."
-    print("python is " + true)
-myfunction()
-print("python is " + true)
+import random
+print(random.randrange(1,50))
